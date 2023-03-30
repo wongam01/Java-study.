daf0e2323a9835e9fe977690619b99aca721f238
wongam <wongam01@naver.com>
2023-03-30 17:53:23 +0900

    message

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,14 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("hello world");
     }
+    /*
+    git push
+    git add . 1
+    git branch 브랜치 이름
+    git checkout 이동 명령어
+    git commit -m "message" 2
+    git push 3  (업로드)
+    */
 }
